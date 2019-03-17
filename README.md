955c7405e53206ef441a7f0afe65be915d790150
Sargun Vohra <sargun.vohra@gmail.com>
2019-03-16 22:19:22 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+[![Downloads](http://cf.way2muchnoise.eu/always-drop-loot.svg)][CurseForge]
+[![CurseForge](http://cf.way2muchnoise.eu/versions/always-drop-loot.svg)][CurseForge]
+[![CircleCI](https://circleci.com/gh/sargunv/always-drop-loot.svg?style=shield)][CircleCI]
+
+# Always Drop Loot
+
+Make mobs always drop exp and loot, regardless of cause of death.
+
+## Information
+
+Check out this mod on [CurseForge][].
+
+## Building from source
+
+```bash
+git clone https://github.com/sargunv/always-drop-loot.git
+cd always-drop-loot
+./gradlew setupDecompWorkspace build runClient
+# On Windows, use "gradlew.bat" instead of "gradlew"
+```
+
+[CurseForge]: https://minecraft.curseforge.com/projects/always-drop-loot
+[CircleCI]: (https://circleci.com/gh/sargunv/always-drop-loot)
---
+++ src/main/java/me/sargunvohra/alwaysdroploot/AlwaysDropLootMod.java
@@ -0,0 +1,32 @@
+/* Licensed under Apache-2.0 */
+package me.sargunvohra.alwaysdroploot;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Mod(modid = AlwaysDropLootMod.MOD_ID, useMetadata = true)
+public class AlwaysDropLootMod {
+  static final String MOD_ID = "@MOD_ID@";
+
+  @EventHandler
+  public void preInit(FMLPreInitializationEvent event) {
+    MinecraftForge.EVENT_BUS.register(this);
+  }
+
+  @SubscribeEvent
+  void onLivingDeath(LivingDeathEvent event) {
+    Entity entity = event.getEntity();
+    if (entity instanceof EntityLivingBase) {
+      EntityLivingBase entityLivingBase = ((EntityLivingBase) entity);
+      if (entityLivingBase.recentlyHit <= 0) {
+        entityLivingBase.recentlyHit = 100;
+      }
+    }
+  }
+}
